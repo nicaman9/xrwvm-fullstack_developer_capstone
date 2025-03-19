f261c9ee62c99f0638d57c564ab086bf77494c9a
nicaman9 <161006273+nicaman9@users.noreply.github.com>
2025-03-19 04:19:48 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,9 +15,12 @@ urlpatterns = [
     # path for car list
     path(route='get_cars', view=views.get_cars, name='getcars'),
     # path for dealers list
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='get_dealers/', view=views.get_dealerships,
+         name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships,
+         name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details,
+         name='dealer_details'),
     # path for dealer reviews view
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
