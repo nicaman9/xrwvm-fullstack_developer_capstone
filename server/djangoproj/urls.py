6722cab1d7d5296551a7b235662190c2fceef9b2
nicaman9 <nicaman9@gmail.com>
2025-03-20 03:30:14 -0400

    updates for pages

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,6 +19,7 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
@@ -30,6 +31,5 @@ urlpatterns = [
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('dealer/<int:dealer_id>',
          TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',
-         TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
